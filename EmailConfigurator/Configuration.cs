93b3ba94ea84acaf15d8190af2a9fc36f12438c2
RichardDanvers <richardjd@hotmail.co.uk>
2019-07-13 14:07:23 +0100

    Added configuration class and base method

---
+++ EmailConfigurator/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace EmailConfigurator
+{
+    public class Configuration
+    {
+        public string SmtpHost { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Ssl { get; set; }
+    }
+}
--- EmailConfigurator/Email.cs
+++ EmailConfigurator/Email.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace EmailConfigurator
 {
-    public class Email
+    public static class Email
     {
+        public static void Send(MailMessage message, Configuration settings)
+        {
+
+            SmtpClient client = new SmtpClient
+            {
+                EnableSsl = settings.Ssl,
+                Credentials = (ICredentialsByHost)new System.Net.NetworkCredential(settings.Username, settings.Password),
+                Host = settings.SmtpHost
+            };
+
+            client.Send(message);
+        }
+
     }
 }
